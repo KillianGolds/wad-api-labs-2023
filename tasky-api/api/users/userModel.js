b1280bbb4532b10094c1f100dd98cd0429fab9f8
KillianGolds <20070585@mail.wit.ie>
2023-12-13 01:12:36 +0000

    Mongo Lab Exercises Done exercise 2 done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -2,9 +2,21 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!$%^&*])[A-Za-z\d!$%^&*]{8,}$/;
+
+//validator function for the password
+const passwordValidator = (password) => {
+  return passwordRegex.test(password);
+};
+
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true,
+    validate: {
+      validator: passwordValidator,
+      message: 'Password does not meet the complexity requirements.'
+    }
+  }
 });
 
 export default mongoose.model('User', UserSchema);
